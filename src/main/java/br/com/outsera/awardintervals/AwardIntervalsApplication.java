acb8fb5196921ce20d49bb91ed9f9043ca5c1203
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2026-05-16 20:33:10 -0300

    chore: scaffold Spring Boot project

---
+++ src/main/java/br/com/outsera/awardintervals/AwardIntervalsApplication.java
@@ -0,0 +1,13 @@
+package br.com.outsera.awardintervals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwardIntervalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwardIntervalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/outsera/awardintervals/AwardIntervalsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.outsera.awardintervals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwardIntervalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
